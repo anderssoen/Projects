c89a684af7219f19569ad71b03c784c369109111
Anderssoen <j_anderssoen@live.se>
2015-01-08 21:44:07 +0100

    Unity Projects

---
+++ Game/Assets/Scripts/CoinRotate.js
@@ -0,0 +1,6 @@
+﻿#pragma strict
+
+function Update () 
+{
+    transform.Rotate(Vector3(0,-3,0));
+}
---
+++ Pong/Assets/Scripts/AIController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour
+{
+    Transform Enemy;
+
+    void Start()
+    {
+        Enemy = GameObject.Find("Ball").transform;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3
+            (8.0f,
+             Enemy.position.y,
+             transform.position.z);
+    }
+}
