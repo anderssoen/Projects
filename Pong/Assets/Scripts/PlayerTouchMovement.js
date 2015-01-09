a42a8e4b2b7c2a53999fa54287fd9fc16bff164b
Anderssoen <j_anderssoen@live.se>
2015-01-09 16:52:15 +0100

    Unity projects

---
+++ Pong/Assets/Scripts/PlayerTouchMovement.js
@@ -0,0 +1,31 @@
+﻿var speed : float = 10;
+ 
+ 
+function Update () {
+ 
+    if (Input.touchCount > 0) 
+    {
+        var touchDeltaPos:Vector2 = Input.GetTouch(0).position;
+        if(Input.touchCount>1)
+            var touchDeltaPos2:Vector2 = Input.GetTouch(1).position;
+        if(touchDeltaPos.x<Screen.width/2)
+        {
+            if(touchDeltaPos.y > Screen.height/2)
+            {
+                rigidbody2D.velocity.y = 1*speed;
+            }
+            else rigidbody2D.velocity.y = -1*speed;
+        }
+        else if(touchDeltaPos2.x<Screen.width/2&&Input.touchCount>1)
+        {
+            if(touchDeltaPos2.y > Screen.height/2)
+            {
+                rigidbody2D.velocity.y = 1*speed;
+            }
+            else rigidbody2D.velocity.y = -1*speed;
+        }
+    }
+    if (Input.touchCount == 0)
+        rigidbody2D.velocity.y = 0;
+    rigidbody2D.velocity.x=0;
+}
